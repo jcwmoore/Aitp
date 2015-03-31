a7fd3e031c961c39986eb358d128436503645373
John Moore <jcwmoore@gmail.com>
2015-03-31 15:13:10 -0500

    adding missing files

---
+++ Aitp.Example.Tests/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Aitp.Example.Tests
+{
+    [TestClass]
+    public class ProgramTest
+    {
+        [TestMethod]
+        public void Program1BreaksTest()
+        {
+            var container = new IoC.Container();
+
+            // create a fake repository
+            var repo = Mock.Of<IRepository<Outage>>();
+            
+            // tell the fake object to fail when Insert method is called
+            Mock.Get(repo).Setup(m => m.Insert(It.IsAny<Outage>())).Throws<Exception>();
+            
+            // put the faked object in the Factory
+            container.Register<IRepository<Outage>>().To(repo);
+
+            // Run the test
+            Program1.Execute(container);
+        }
+
+
+
+
+        [TestMethod]
+        public void Program2BreaksTest()
+        {
+            var container = new IoC.Container();
+            var repo = Mock.Of<IRepository<Outage>>();
+            Mock.Get(repo).Setup(m => m.Insert(It.IsAny<Outage>())).Throws<Exception>();
+            container.Register<IRepository<Outage>>().To(repo);
+
+            Program2.Execute(container);
+        }
+    }
+}
---
+++ Aitp.Example/Outage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aitp.Example
+{
+    public class Outage
+    {
+        public DateTime StartTimeUtc { get; set; }
+        public DateTime EndTimeUtc { get; set; }
+        public string UnitName { get; set; }
+    }
+}
